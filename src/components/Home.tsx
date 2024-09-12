e8a58b85b9dba980d645eb3e35f144eacc3bda9d
sushil016 <sahanisushil325@gmail.com>
2024-09-12 23:52:27 +0530

    customize cursor

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,8 @@
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import InstagramIcon from '@mui/icons-material/Instagram';
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import Link from "next/link";
 
 interface HomeProps {
   bgVariant: string;
@@ -10,39 +11,72 @@ interface HomeProps {
   setCursorText: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const Home: React.FC<HomeProps> = ({ setBgVariant, setCursorText })=> {
-  function textEnter(){
-    setBgVariant("nameCursor")
-    setCursorText("Name")
+const Home: React.FC<HomeProps> = ({ setBgVariant, setCursorText }) => {
+  function textEnter() {
+    setBgVariant("nameCursor");
+    setCursorText("");
   }
 
-  function textOut(){
-   setBgVariant("default")
-   setCursorText("")
+  function textOut() {
+    setBgVariant("default");
+    setCursorText("");
   }
-  
+
   return (
     <>
       <div className="bg-[#1E1E2E] h-screen w-full flex">
         <div className="w-1/2 h-screen flex justify-center items-center flex-col">
-          <div 
-          onMouseEnter={textEnter}
-          onMouseLeave={textOut}
-           className="text-[#B2DBF2] text-center text-[80px] font-bold pointer-events ">
+          <div
+            onMouseEnter={textEnter}
+            onMouseLeave={textOut}
+            className="text-[#B2DBF2] text-center text-[80px] font-bold pointer-events "
+          >
             Sushil Here!
           </div>
           <div className="text-[#CAA6F7] text-center text-[30px] font-bold ml-[40px] mt-6 ">
             Full Stack Devloper | Open source
           </div>
           <div className="text-[#CAA6F7] text-center text-[30px] font-bold ml-[60px] w-[600px]  p-8">
-            Feel free to tweak it to match 
-            your style!
+            Feel free to tweak it to match your style!
           </div>
-          <div className="flex  bg-slate-900 p-4 rounded-full gap-12"> 
-            <div><button><GitHubIcon /></button></div>
-            <div><button><LinkedInIcon /></button></div>
-            <div><button><TwitterIcon /></button></div>
-            <div><button><InstagramIcon /></button></div>
+          <div className="flex  bg-slate-900 p-4 rounded-full gap-12">
+            <div>
+              <Link
+                data-popover-target="popover-default"
+                href="https://github.com/sushil016"
+                className="hover:text-white"
+              >
+                <GitHubIcon />
+              </Link>
+              <div
+                data-popover
+                id="popover-default"
+                
+                className="absolute z-10 invisible inline-block w-64 text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 dark:text-gray-400 dark:border-gray-600 dark:bg-gray-800"
+              >
+                <div className="px-3 py-2 bg-gray-100 border-b border-gray-200 rounded-t-lg dark:border-gray-600 dark:bg-gray-700">
+                  <h3 className="font-semibold text-gray-900 dark:text-white">
+                    Github
+                  </h3>
+                </div>
+                <div data-popper-arrow></div>
+              </div>
+            </div>
+            <div>
+              <Link href="https://www.linkedin.com/in/sushil-sahani-46235527b/">
+                <LinkedInIcon />
+              </Link>
+            </div>
+            <div>
+              <Link href="https://x.com/Sushil_Sahani37">
+                <TwitterIcon />
+              </Link>
+            </div>
+            <div>
+              <Link href="">
+                <InstagramIcon />
+              </Link>
+            </div>
           </div>
         </div>
         <div className="text-white text-center h-full w-1/2 text-2xl font-bold flex justify-center items-center bg-slate-800">
@@ -50,7 +84,11 @@ const Home: React.FC<HomeProps> = ({ setBgVariant, setCursorText })=> {
         </div>
       </div>
       <div className="bg-[#353576] h-screen w-full ">
-        This is the home page baby{" "}
+        {/* <div className="flex w-[200px] h-[200px] justify-center items-center relative">
+          <div className="w-[110%] h-[110%] shadow-lg shadow-pink-500 bg-transparent rounded-full absolute"></div>
+          <div className="w-[108%] h-[108%] shadow-lg shadow-violet-500 bg-transparent rounded-full absolute rotate-90"></div>
+          <div className="w-[106%] h-[106%] shadow-lg shadow-cyan-500 bg-transparent rounded-full absolute rotate-180"></div>
+        </div> */}
       </div>
     </>
   );
--- src/components/portfolio.tsx
+++ src/components/portfolio.tsx
@@ -1,71 +1,82 @@
-"use client"
-import React from 'react'
-import Navbar from './Navbar'
-import Home from './Home'
-import {motion} from 'framer-motion'
+"use client";
+import React from "react";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import { motion } from "framer-motion";
 
 import { useState } from "react";
-import useMousePosition from '@/app/utils/mouseposition';
+import useMousePosition from "@/app/utils/mouseposition";
 
 const LandingPage = () => {
-
-  
-  const {x,y} = useMousePosition();
-  const [bgVariant, setBgVariant] = useState("default")
+  const { x, y } = useMousePosition();
+  const [bgVariant, setBgVariant] = useState("default");
 
   const [cursorText, setCursorText] = useState("");
 
-
-
   const variants = {
     default: {
-      x:x-20,
-      y:y-20,
+      x: x - 20,
+      y: y - 20,
       transition: {
         type: "spring",
-        mass: 0.6
-      }
+        mass: 0.6,
+      },
     },
 
-    navbarCursor:{
-      opacity:1,
-      width:60,
-      height:60,
-      border:"none",
-      backgroundColor:"#FFA500",
-      mixBlendMode:"difference" as const,
-      x:x-30,
-      y:y-30,
+    navbarCursor: {
+      opacity: 1,
+      width: 60,
+      height: 60,
+      border: "none",
+      backgroundColor: "#FFA500",
+      mixBlendMode: "difference" as const,
+      x: x - 30,
+      y: y - 30,
     },
-    nameCursor:{
-      opacity:1,
-      width:100,
-      height:100,
+    nameCursor: {
+      opacity: 1,
+      width: 100,
+      height: 100,
       // border:"none",
-      backgroundColor:"#1E1E2E",
-      mixBlendMode:"difference" as const,
-      x:x-50,
-      y:y-50,
-    }
-  }
+      backgroundColor: "#1E1E2E",
+      mixBlendMode: "difference" as const,
+      x: x - 50,
+      y: y - 50,
+      cursor: "none",
+    },
+  };
 
   const spring = {
     type: "spring",
     stiffness: 500,
-    damping: 28
+    damping: 28,
   };
-  
-  return (
-    <> 
-       <div>
-       <motion.div variants={variants} transition={spring} animate={bgVariant}  className="cursor-none z-10 h-[40px] w-[40px] rounded-full border-2 border-orange-600 fixed top-0 left-0 pointer-events-none justify-center items-center">
-       <span className='text-white m-auto pointer-events-none font-size-inherit flex justify-center items-center mt-9'>{cursorText}</span>
-       </motion.div>
-       </div>
-      <Navbar bgVariant={bgVariant} setBgVarient={setBgVariant}/>
-      <Home bgVariant={bgVariant} setBgVariant={setBgVariant} cursorText={cursorText} setCursorText={setCursorText}/>
-    </>
-  )
-}
 
-export default LandingPage
+  return (
+    <>
+      <div>
+        <motion.div
+          variants={variants}
+          transition={spring}
+          animate={bgVariant}
+          className="cursor-none z-10 h-[40px] w-[40px] rounded-full border-2 border-orange-600 fixed top-0 left-0 pointer-events-none justify-center items-center shadow-lg shadow-violet-500"
+        > 
+          <span className="text-white m-auto font-size-inherit flex justify-center items-center mt-9">
+            {cursorText}
+          </span>
+        </motion.div>
+      </div>
+      <Navbar bgVariant={bgVariant} setBgVarient={setBgVariant} />
+      <Home
+        bgVariant={bgVariant}
+        setBgVariant={setBgVariant}
+        cursorText={cursorText}
+        setCursorText={setCursorText}
+      />
+    </>
+  );
+};
+
+export default LandingPage;
+
+// className="cursor-none z-10 h-[40px] w-[40px] rounded-full border-2 border-orange-600 fixed top-0 left-0 pointer-events-none justify-center items-center"
